108bb61b1f5da2217ffb47bceb4a06486ff1b6cb
Ewout Quax <ewout.quax@quicknet.nl>
2015-01-07 22:03:57 +0100

    Fix for Rails 4.2.0 support
    The active-record 'select' operation, used to show the tables, is now
    executed via the 'send'-operation

--- lib/db_to_file/unloader.rb
+++ lib/db_to_file/unloader.rb
@@ -3,7 +3,7 @@ module DbToFile
     def initialize
       # Load config and build database connection, before stashing possible changes
       @config ||= load_config
-      ActiveRecord::Base.connection.select('show tables')
+      ActiveRecord::Base.connection.send(:select, 'show tables')
     end
 
     def unload
